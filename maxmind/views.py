7063ec47d15335ee353cb5a16ce29fefca970c61
Sebastian Estenssoro <seb.estenssoro@gmail.com>
2017-07-06 11:14:23 -0600

    basic api view

--- maxmind/views.py
+++ maxmind/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from models import Block, Location
 
 
 def home(request):
     return render(request, 'maxmind/home.html')
+
+
+class GeoDataAPI(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, format=None):
+        blocks = Location.objects.all().order_by('?')[:2000].values()
+        return Response(blocks)
