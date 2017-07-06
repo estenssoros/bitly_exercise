23dd00f66a1efdfa9683166eb88f597ff6b6cb7c
Sebastian Estenssoro <seb.estenssoro@gmail.com>
2017-07-06 11:14:39 -0600

    rest framework urls

--- maxmind/urls.py
+++ maxmind/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 
 import views
 
 urlpatterns = [
     url(r'^$', views.home),
+    url(r'^api/geo_data/$', views.GeoDataAPI.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
