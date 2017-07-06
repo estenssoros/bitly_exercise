ea425b8c0b921dffd1ccf944924acf9974eeea63
Sebastian Estenssoro <seb.estenssoro@gmail.com>
2017-07-06 09:07:28 -0600

    race averages and django view

--- race_average/views.py
+++ race_average/views.py
@@ -1,6 +1,20 @@
+import os
+
+from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from scripts.race_averages import RaceAverage
+
 
 def home(request):
-    return render(request, 'race_average/home.html')
+    context = {'race_start': '08:00 AM, DAY 1'}
+    race_average = RaceAverage(**context)
+    test_dir = os.path.join(settings.BASE_DIR, 'race_average', 'scripts', 'tests')
+    test_strings = []
+    for f in os.listdir(test_dir):
+        times = [line.strip() for line in open(os.path.join(test_dir, f))]
+        test_strings.append({'string': str(times),
+                             'result': race_average.avgMinutes(times)})
+    context['test_strings'] = test_strings
+    return render(request, 'race_average/home.html', context)
