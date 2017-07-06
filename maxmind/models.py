0eba635016ceece163536c57c9e91dbbc1774371
Sebastian Estenssoro <seb.estenssoro@gmail.com>
2017-07-06 11:14:47 -0600

    maxmind models

--- maxmind/models.py
+++ maxmind/models.py
@@ -2,4 +2,32 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Block(models.Model):
+    id = models.IntegerField(primary_key=True)
+    start_ip_num = models.IntegerField()
+    end_ip_num = models.IntegerField()
+    loc_id = models.IntegerField()
+
+    class Meta:
+        db_table = 'blocks'
+        verbose_name = 'Block'
+        verbose_name_plural = 'Blocks'
+
+
+class Location(models.Model):
+    id = models.IntegerField(primary_key=True)
+    loc_id = models.IntegerField()
+    country = models.CharField(max_length=2, blank=True, null=True)
+    region = models.CharField(max_length=2, blank=True, null=True)
+    city = models.CharField(max_length=48, blank=True, null=True)
+    postal_code = models.CharField(max_length=8, blank=True, null=True)
+    latitude = models.DecimalField(max_digits=8, decimal_places=6, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=8, decimal_places=6, blank=True, null=True)
+    metro_code = models.IntegerField()
+    area_code = models.IntegerField()
+
+    class Meta:
+        db_table = 'location'
+        verbose_name = 'Location'
+        verbose_name_plural = 'Locations'
