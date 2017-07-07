497007afcb02e0d42e4dd58a356d000f0ae1cfcf
Sebastian Estenssoro <seb.estenssoro@gmail.com>
2017-07-07 07:53:07 -0600

    added loc detail and loc api urls

--- maxmind/urls.py
+++ maxmind/urls.py
@@ -5,7 +5,9 @@ import views
 
 urlpatterns = [
     url(r'^$', views.home),
+    url(r'^loc_detail/(?P<loc_id>[0-9.]+)/$', views.loc_detail),
     url(r'^api/geo_data/$', views.GeoDataAPI.as_view()),
+    url(r'^api/loc_data/$', views.LocAPI.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
